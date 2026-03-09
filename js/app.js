0c5c67fd66be5e261a018818c4deaa310fe01d6f
bruno dykstra <brunodykstra@gmail.com>
2026-03-09 15:57:33 -0300

    atualização

--- js/app.js
+++ js/app.js
@@ -7,13 +7,15 @@ const gastosPorCategoria = new ListaGastosCategorias(
     new Categoria('Moradia'),
     new Categoria('Lazer'),
     new Categoria('Outros'),
-)
+);
+
 const formulario = document.querySelector('form');
 
 formulario.addEventListener('submit', (evento) => {
     evento.preventDefault();
-        const valorInformado = formulario.elements.valor.value;
-        const categoriaInformada = formulario.elements.categoria.value;
+
+    const valorInformado = formulario.elements.valor.value;
+    const categoriaInformada = formulario.elements.categoria.value;
 
     if (valorNegativo(valorInformado)) {
         alert("Por favor, insira um valor válido para o gasto.");
@@ -23,26 +25,6 @@ formulario.addEventListener('submit', (evento) => {
     const categoria = gastosPorCategoria.obterCategoria(categoriaInformada);
     categoria.adicionarValor(valorInformado);
 
-
     atualizarInterface(gastosPorCategoria);
     formulario.reset();
 });
-
-
-function adicionarGasto() {
-
-    const valorInformado = obterValorInformado();
-    const categoriaInformada = obterCategoriaInformada();
-
-    if (valorNegativo(valorInformado)) {
-        alert("Por favor, insira um valor válido para o gasto.");
-        return;
-    }
-    const categoria = obterCategoria(matrizGastos, categoriaInformada);
-    const total = obterCategoria(matrizGastos, "Total");
-
-    atualizarValorCategoria(categoria, valorInformado);
-    atualizarValorCategoria(total, valorInformado);
-    atualizarInterface();
-    limparCampos();
-}
